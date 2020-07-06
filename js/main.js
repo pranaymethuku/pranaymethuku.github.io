23e3afca11f1abb0741acf5eec4515ddce07ce23
Pranay Methuku <33403387+pranaymethuku@users.noreply.github.com>
2020-07-05 23:20:42 -0400

    Update GT to terminal

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(() => {
 
 	// this should work because of typed.js
 	var terminal = new Typed('#terminal', {
-		strings: ['Hello!', 'I am a student at Ohio State.', 'I love solving problems.', "It's nice to meet you :)"],
+		strings: ['Hello!', 'I am a student at Georgia Tech.', 'I am an Ohio State alumni.', 'I love solving problems.', "It's nice to meet you :)"],
 		typeSpeed: 50,
 		backSpeed: 10,
 		cursorChar: '&#9608;',
